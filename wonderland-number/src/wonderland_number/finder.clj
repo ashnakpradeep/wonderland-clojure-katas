5c85c163d1f2da4bf3ee67193692b0f2329e8e0e
ashnakpradeep <ashnakpradeep@gmail.com>
2019-01-07 14:48:03 +0530

    wonderland number

--- wonderland-number/src/wonderland_number/finder.clj
+++ wonderland-number/src/wonderland_number/finder.clj
@@ -1,5 +1,18 @@
 (ns wonderland-number.finder)
 
+(defn equaldigits [a b]
+  (= (set (str a)) (set (str b))))
+
+(defn wondernumber [x]
+ (and
+  (equaldigits x (* 2 x))
+  (equaldigits x (* 3 x))
+  (equaldigits x (* 4 x))
+  (equaldigits x (* 5 x))
+  (equaldigits x (* 6 x))))
+
 (defn wonderland-number []
-  ;; calculate me
-  42)
+  
+(first (filter wondernumber (range 100000 1000000))))
+
+
