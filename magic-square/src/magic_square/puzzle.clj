01ef35a7d0500653437eca0fd4e88f7aa9e15291
ashnakpradeep <ashnakpradeep@gmail.com>
2019-01-08 18:07:22 +0530

    magic square

--- magic-square/src/magic_square/puzzle.clj
+++ magic-square/src/magic_square/puzzle.clj
@@ -1,25 +1,18 @@
 (ns magic-square.puzzle)
+(def values [1.0 1.5 2.0 2.5 3.0 3.5 4.0 4.5 5.0])
 
 (defn magic-square [values]
-  [[1.0 1.5 2.0]
-   [2.5 3.0 3.5]
-   [4.0 4.5 5.0]]
-   (def sumvalue (reduce + values))
-   (def magicno (/ sumvalue 3))
-   (def me (int (/ (count values) 2)))
-   (let [
-         mm (get values me)
-         mf (get values (+ me 3))
-         mb (get values (- me 3))
-         mr (get values (+ me 1))
-         mc (get values (- me 1))
-         r1 (- magicno (+ mr mf))
-         r3 (- magicno (+ mc mb))
-         c1 (- magicno (+ mr mb))
-         c3 (- magicno (+ mc mf))]
-     (def result [[mr r1 mf]
-                  [c1 mm c3]
-                  [mb r3 mc]])
+ (let[  sumvalue (reduce + values)
+          magicno (/ sumvalue 3)
+            me (int (/ (count values) 2))
+            mm (get values me)
+            mf (get values (+ me 3))
+            mb (get values (- me 3))
+            mr (get values (+ me 1))
+            mc (get values (- me 1))]
+           (def  result [[mr (- magicno (+ mr mf)) mf]
+                 [(- magicno (+ mr mb))  mm (- magicno (+ mc mf))]
+                 [mb (- magicno (+ mc mb)) mc]])
   )result)
          
    
--- magic-square/test/magic_square/puzzle_test.clj
+++ magic-square/test/magic_square/puzzle_test.clj
@@ -5,7 +5,6 @@
 (defn sum-rows [m]
   (map #(reduce + %) m))
 
-(def values [1.0 1.5 2.0 2.5 3.0 3.5 4.0 4.5 5.0])
 
 (defn sum-cols [m]
   [(reduce + (map first m))
