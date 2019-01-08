4c8e826ce10ded091d174843d69d330878cd11f6
ashnakpradeep <ashnakpradeep@gmail.com>
2019-01-08 18:01:27 +0530

    doublets

--- doublets/src/doublets/solver.clj
+++ doublets/src/doublets/solver.clj
@@ -11,7 +11,7 @@
 
  
 (defn doublereg  [dat org word1 word2]
-   (def nr(filter #(== (count  word1) (count %))words))
+   (let [ nr(filter #(== (count  word1) (count %))words)]
   (if (=  word1  word2)
     (conj dat word1)
     (let [ops (->> nr
@@ -22,9 +22,8 @@
                   (map first))
                   w1(conj dat word1)
                   w2 (conj org  word1)]
-;      (if (empty? ops)
-;        []
+
         (letfn [(f [word] (doublereg w1 w2 word  word2))]
               
-(first (filter (comp not empty?) (map f ops)))))))
+(first (filter (comp not empty?) (map f ops))))))))
 (defn doublets [word1 word2] (doublereg [] #{} word1 word2))
