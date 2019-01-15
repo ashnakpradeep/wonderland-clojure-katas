0f0a04e8a751f0d908134515fcea4ae23bd61d62
ashnakpradeep <ashnakpradeep@gmail.com>
2019-01-15 16:41:35 +0530

    tinymaze

--- fox-goose-bag-of-corn/src/fox_goose_bag_of_corn/puzzle.clj
+++ fox-goose-bag-of-corn/src/fox_goose_bag_of_corn/puzzle.clj
@@ -1,6 +1,75 @@
-(ns fox-goose-bag-of-corn.puzzle)
+(ns fox-goose-bag-of-corn.puzzle
+  (:require [clojure.set :as set]
+            ))
 
 (def start-pos [[[:fox :goose :corn :you] [:boat] []]])
 
-(defn river-crossing-plan []
-  start-pos)
+(defn end-pos [last-item]
+  (= last-item [#{} #{:boat} #{:fox :goose :corn :you}]))
+
+
+(defn left-side-take [left-bank-state]
+  (cond
+    (<= (count (clojure.set/difference left-bank-state #{:you})) 1) left-bank-state
+    
+    (clojure.set/subset? #{:goose :fox :corn} left-bank-state) #{:goose :you}
+    
+    (contains? left-bank-state :goose) (clojure.set/difference left-bank-state #{:goose})
+    
+    :otherwise #{:you (first (clojure.set/difference left-bank-state #{:you}))}
+    )
+  )
+
+
+(defn right-side-take [right-bank-state]
+  (cond
+    (<= (count (clojure.set/difference right-bank-state #{:you})) 1) #{:you}
+    
+    (not (contains? right-bank-state :goose)) #{:you}
+    
+    :otherwise #{:you :goose}
+    )
+  )
+
+(defn find-next-move [last-item forward?]
+  
+(let [left (first last-item)
+      middle (second last-item)
+      right (last last-item)
+      remaining (clojure.set/difference middle #{:boat})]
+  (cond
+    (contains? middle :you ) (if forward? {:forward false  :move [left #{:boat} (clojure.set/union right remaining)]}
+                                             {:forward true :move [(clojure.set/union left remaining) #{:boat} right]})
+    (contains? left :you) (let [to-take (left-side-take left)]
+                            {:forward true :move [(clojure.set/difference left to-take)
+                                                  (clojure.set/union middle to-take)
+                                                  right]})
+    :otherwise (let [to-take (right-side-take right)]
+                 {:forward false :move [left
+                                        (clojure.set/union middle to-take)
+                                        (clojure.set/difference right to-take)]})
+    )
+
+  ) )
+
+
+
+
+
+(defn crossing [last-item]
+ 
+ 
+    (loop [out [(last-item start-pos)] forward? true]
+
+     (if (end-pos (last-item out))
+       (map vec out)
+    
+       (let [{forward :forward move :move} (find-next-move (last-item out) forward? )]
+        
+         (recur (conj out move) forward)
+         )
+       )))
+
+(defn river-crossing-plan[]
+  (let [last-item #(map set (last %))]
+    (crossing last-item)))
--- tiny-maze/src/tiny_maze/solver.clj
+++ tiny-maze/src/tiny_maze/solver.clj
@@ -1,3 +1,35 @@
 (ns tiny-maze.solver)
 
-(defn solve-maze [maze])
+(def maze [[:S 0 0 1]
+          [1  1 0 0]
+          [1  0  0 1]
+          [1  1  0 :E]])
+
+(defn positions [maze v]
+  (first (for [[row-idx rowval] (map-indexed vector maze)
+               [col-idx colval] (map-indexed vector rowval)
+               :when (= colval v)]
+           [row-idx col-idx])))
+
+(defn neighbours [maze [row col]]
+  (->> (list [(inc row) col] [(dec row) col] [row (inc col)] [row (dec col)])
+       (filter #(or (= 0 (get-in maze %)) (= :E (get-in maze %))))))
+
+(defn solving [maze points]
+  (if-let [p (first points)]
+
+    (let [sidepoints (neighbours maze p)]
+      (solving (reduce #(assoc-in %1 %2 p) maze sidepoints) (concat (next points) sidepoints)))
+    maze))
+
+
+(defn calculation [maze]
+  (let [starts  [(positions maze :S)]
+        solved (solving maze starts)
+        ends (positions maze :E)
+        ]
+     (reduce #(assoc-in %1 %2 :x) maze (take-while vector? (iterate #(get-in solved %) ends)))))
+
+(defn solve-maze [maze]
+  (calculation maze))
+  
